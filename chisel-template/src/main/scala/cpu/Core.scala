11d21740ccc6bd455eba6c382d14779f04b388f3
masatora <atarashi.masatora@gmail.com>
2021-11-24 00:05:43 +0900

    add logical

--- chisel-template/src/main/scala/cpu/Core.scala
+++ chisel-template/src/main/scala/cpu/Core.scala
@@ -37,7 +37,13 @@ class Core extends Module {
     (inst === LW || inst === ADDI) -> (rs1_data + imm_i_sext),
     (inst === SW) -> (rs1_data + imm_s_sext),
     (inst === ADD) -> (rs1_data + rs2_data),
-    (inst === SUB) -> (rs1_data - rs2_data)
+    (inst === SUB) -> (rs1_data - rs2_data),
+    (inst === ADD) -> (rs1_data & rs2_data),
+    (inst === OR) -> (rs1_data | rs2_data),
+    (inst === XOR) -> (rs1_data ^ rs2_data),
+    (inst === ADDI) -> (rs1_data & imm_i_sext),
+    (inst === ORI) -> (rs1_data | imm_i_sext),
+    (inst === XORI) -> (rs1_data ^ imm_i_sext),
   ))
 
   // MEM Stage
@@ -49,7 +55,10 @@ class Core extends Module {
   val wb_data = MuxCase(alu_out, Seq(
     (inst === LW) -> io.dmem.rdata
   ))
-  when(inst === LW || inst === ADD || inst === ADDI || inst === SUB) {
+  when(
+    inst === LW || inst === ADD || inst === ADDI || inst === SUB || inst === AND || inst === OR ||
+      inst === XOR || inst === ANDI || inst === ORI || inst === XORI
+  ) {
     regfile(wb_addr) := wb_data
   }
 
