f42a247cb7422917b5c8b8bdd1f9b92bad0cf511
masatora <atarashi.masatora@gmail.com>
2021-11-23 22:45:24 +0900

    add load word

--- chisel-template/src/main/scala/cpu/Core.scala
+++ chisel-template/src/main/scala/cpu/Core.scala
@@ -1,16 +1,20 @@
 package cpu
 
 import chisel3._
+import chisel3.util._
 import common.Consts._
+import common.Instructions._
 
 class Core extends Module {
   val io = IO(new Bundle {
     val imem = Flipped(new ImemPortIo())
+    val dmem = Flipped(new DmemPortIo())
     val exit = Output(Bool())
   })
 
   val regfile = Mem(32, UInt(WORD_LEN.W))
 
+  //**********************************
   // IF Stage
   val pc_reg = RegInit(START_ADDR)
   pc_reg := pc_reg + 4.U(WORD_LEN.W)
@@ -23,8 +27,26 @@ class Core extends Module {
   val wb_addr = inst(11, 7)
   val rs1_data = Mux((rs1_addr =/= 0.U(WORD_LEN.U)), regfile(rs1_addr), 0.U(WORD_LEN.W))
   val rs2_data = Mux((rs2_addr =/= 0.U(WORD_LEN.U)), regfile(rs2_addr), 0.U(WORD_LEN.W))
+  val imm_i = inst(31, 20)
+  val imm_i_sext = Cat(Fill(20, imm_i(11)), imm_i)
 
-  io.exit := (inst === 0x34333231.U(WORD_LEN.W))
+  // EX Stage
+  val alu_out = MuxCase(0.U(WORD_LEN.W), Seq(
+    (inst === LW) -> (rs1_data + imm_i_sext)
+  ))
+
+  // Memory Access Stage
+  io.dmem.addr := alu_out
+
+  // WB Stage
+  val wb_data = io.dmem.rdata
+  when(inst === LW) {
+    regfile(wb_addr) := wb_data
+  }
+
+  //**********************************
+  // Debug
+  io.exit := (inst === 0x14131211.U(WORD_LEN.W))
   printf(p"pc_reg: 0x${Hexadecimal(pc_reg)}\n")
   printf(p"list: 0x${Hexadecimal(inst)}\n")
   printf(p"rs1_addr: $rs1_addr\n")
@@ -32,6 +54,7 @@ class Core extends Module {
   printf(p"wb_addr: $wb_addr\n")
   printf(p"rs1_data: 0x${Hexadecimal(rs1_data)}\n")
   printf(p"rs2_data: 0x${Hexadecimal(rs2_data)}\n")
-
+  printf(p"wb_data: 0x${Hexadecimal(wb_data)}\n")
+  printf(p"dmem.addr: ${io.dmem.addr}\n")
   printf("-------------\n")
 }
